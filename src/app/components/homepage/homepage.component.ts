372132a813bb07701e9ef0fd72f0e97dc96adabc
proGamalSherif <jemyxsherifx@gmail.com>
2025-03-30 15:48:49 +0200

    Updates on Homepage

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,21 +1,22 @@
-import { Component } from '@angular/core';
-import { ProductCardComponent } from "../product-card/product-card.component";
-import { CategoryCardComponent } from "../category-card/category-card.component";
-import { BackendAPIService } from '../../services/backend-api.service';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'
+import { Product } from '../../models/product';
+import { ProductService } from '../../services/product.service';
+import { ProductCardComponent } from "../product-card/product-card.component";
 @Component({
   standalone: true,
   selector: 'app-homepage',
-  imports: [FormsModule, CommonModule, ProductCardComponent, CategoryCardComponent],
+  imports: [FormsModule, CommonModule, ProductCardComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
-export class HomepageComponent {
-  FullProductList!:any;
-  constructor(private apiServ: BackendAPIService) { 
-    this.apiServ.GetEntity('Product').subscribe(res=>{
-      this.FullProductList=res;
+export class HomepageComponent implements OnInit {
+  ProductArr!:Product[];
+  constructor(private productServ:ProductService) { }
+  ngOnInit() {
+    this.productServ.getAllProducts().subscribe(res=>{
+      this.ProductArr=res;
     })
   }
 }
