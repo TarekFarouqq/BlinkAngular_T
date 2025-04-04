49f8eb70f44814dc3b87249f1ad9c5d2abff1799
proGamalSherif <jemyxsherifx@gmail.com>
2025-04-04 18:54:00 +0200

    last updates

---
+++ src/app/components/new-navbar/new-navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-navbar',
+  imports: [],
+  templateUrl: './new-navbar.component.html',
+  styleUrl: './new-navbar.component.css'
+})
+export class NewNavbarComponent {
+
+}
