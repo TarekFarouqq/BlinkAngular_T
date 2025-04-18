e5feb25c2a0986bc87e7fbdba9918081e063131e
Abdelazeezz <abdelazezmahmoud59@gmail.com>
2025-04-18 06:39:32 +0200

    Create Loading Screen For Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth.service';
+import { NgxSpinnerComponent } from 'ngx-spinner';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgxSpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { headerInterceptor } from './Interceptors/header.interceptor';
+import { NgxSpinnerModule } from "ngx-spinner";
+import { loadingInterceptor } from './Interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(withInterceptors([headerInterceptor]))
+    provideHttpClient(withInterceptors([headerInterceptor,loadingInterceptor])),
+    importProvidersFrom(NgxSpinnerModule ),
+    
   ]
 };
