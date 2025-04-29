35313780742a44ece3727331772f12955f73e3e2
tarekfarouqq <tarekahmedfarouq@gmail.com>
2025-04-29 11:45:07 +0300

    change api source

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-       // apiUrl: 'https://localhost:7027/api'                       //local 
+       apiUrl: 'https://localhost:7027/api'                       //local 
     // apiUrl: 'http://blink-smallapi.runasp.net/api'             //small
-    apiUrl: 'http://blink-largeapi.runasp.net/api'                //lagre         
+    // apiUrl: 'http://blink-largeapi.runasp.net/api'                //lagre         
 };
